bfcf4aeac78f89f790337eb9a6c1b4e88b0bdb38
omardtl24 <otoledo@unal.edu.co>
2025-11-15 22:02:35 -0500

    Ancizar font + Colors

---
+++ frontend/src/theme/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+  primary: {
+    cyan: "#6DCFD9",
+    blue: "#4d80c9",
+  },
+  secondary: {
+    "cyan-accent": "#EAFDFF",
+  },
+};
+
+export default colors;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+import colors from "./src/theme/colors.js";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ancizar: ["AncizarSans", "sans-serif"],
+        ancizarItalic: ["AncizarSansItalic", "sans-serif"],
+      },
+      colors: {
+        primary: colors.primary,
+        secondary: colors.secondary
+      }
+    },
   },
   plugins: [],
 };
