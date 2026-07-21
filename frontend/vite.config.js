e767fee50c249fe19c276d86343965eac16852d7
omardtl24 <otoledo@una.edu.co>
2026-07-21 15:48:41 -0500

    fix: vite server + envs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,13 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
+  base: "/app-observatorio-analisis-indicadores/",
+
   plugins: [
     react(),
     svgr(),
   ],
+
   server: {
     host: true,
     hmr: {
