dbb8a4bdb74b8cdb678cdcbc3e2de6c008616046
omardtl24 <otoledo@una.edu.co>
2026-07-21 19:01:02 -0500

    fix: loop + prefix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/app-observatorio-analisis-indicadores">
       <App />
     </BrowserRouter>
   </React.StrictMode>
